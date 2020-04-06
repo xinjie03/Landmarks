ef619b93ae884c939e10e340d9fcd3affcaa847f
xinjie03 <48872916+xinjie03@users.noreply.github.com>
2020-04-06 19:46:14 +0800

    Add files via upload

---
+++ StartingPoint/Landmarks/Landmarks/LandmarkList.swift
@@ -0,0 +1,33 @@
+
+import SwiftUI
+
+struct LandmarkList: View {
+    @EnvironmentObject var userData: UserData
+
+    var body: some View {
+        NavigationView {
+            List {
+                Toggle(isOn: $userData.showFavoritesOnly) {
+                    Text("Favorites only")
+                }
+
+                ForEach(userData.landmarks) { landmark in
+                    if !self.userData.showFavoritesOnly || landmark.isFavorite {
+                        NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
+                            LandmarkRow(landmark: landmark)
+                        }
+                    }
+                }
+            }
+            .navigationBarTitle(Text("Landmarks"))
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        //Foreach
+        LandmarkList()
+            .environmentObject(UserData())
+    }
+}
---
+++ StartingPoint/Landmarks/Landmarks/UserData.swift
@@ -0,0 +1,8 @@
+
+import SwiftUI
+import Combine
+
+final class UserData: ObservableObject  {
+    @Published var showFavoritesOnly = false
+    @Published var landmarks = landmarkData
+}
